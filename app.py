044660061b698a147872be5aba8515a5b6b46ab8
RamiroTomas <ram_tom12@hotmail.com>
2024-04-21 21:58:24 -0600

    tercer intento

--- app.py
+++ app.py
@@ -11,3 +11,13 @@ if hist_button:
     fig = px.histogram(car_data, x="odometer")
     
     st.plotly_chart(fig, use_container_width=True)
+
+car_data = pd.read_csv('vehicles_us.csv') # leer los datos
+hist_button = st.button('Construir tabla dispersion') # crear un botón
+    
+if hist_button:
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    st.header('Venta de coches')
+    fig = px.scatter(car_data, x="odometer", y="price")
+    
+    st.plotly_chart(fig, use_container_width=True)
