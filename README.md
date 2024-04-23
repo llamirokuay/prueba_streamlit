2deeb2735aa820048fb180070be607b92556ccf5
RamiroTomas <ram_tom12@hotmail.com>
2024-04-22 22:36:37 -0600

    revision3

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Mi proyecto de Streamlit
+
+### El objetivo de este proyecto  es la creación y gestión de entornos virtuales en Python
+
+# Instrucciones para completar el proyecto
+
+### Crea una cuenta en github.com.
+### Crea un nuevo repositorio git con un archivo README.md y un archivo .gitignore
+### Crea una cuenta en render.com.
+### Realizar un análisis exploratorio de datos. Para lograrlo, necesitarás tener instalados los paquetes pandas y plotly-express. 
--- app.py
+++ app.py
@@ -4,20 +4,22 @@ import streamlit as st
         
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
 hist_button = st.button('Construir histograma') # crear un botón
+st.header('Venta de coches')
         
 if hist_button:
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
-    st.header('Venta de coches')
+    
     fig = px.histogram(car_data, x="odometer")
     
     st.plotly_chart(fig, use_container_width=True)
 
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
 hist_button = st.button('Construir tabla dispersion') # crear un botón
+st.header('Grafico dispersion')
     
 if hist_button:
     st.write('Creación de una tabla de dispersion para el conjunto de datos de anuncios de venta de coches')
-    st.header('Grafico dispersion')
+    
     fig = px.scatter(car_data, x="odometer", y="price")
     
     st.plotly_chart(fig, use_container_width=True)
